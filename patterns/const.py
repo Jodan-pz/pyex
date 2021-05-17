3f979e55d99bb1bacbe4f32631e6640d2a6ae997
Daniele Giove <daniele.giove@amundi.com>
2021-05-17 17:06:10 +0200

    dg

--- patterns/const.py
+++ patterns/const.py
@@ -1,13 +1,23 @@
 import calendar
 import os
+from pprint import pprint
 
 a = calendar.MONDAY
 
-print( "%d" % a)
+print("%d" % a)
 
 c = frozenset(range(12))
 
-print ( c )
+print(c)
 
-print ( os.path.dirname(__file__) )
-print  ( __name__  )
+print(os.path.dirname(__file__))
+print(__name__)
+
+loll = {"a": 12, "x": [12, 3, 2, 4], "pol": (12, 3, 4,)}
+pprint(loll, sort_dicts=0)
+print(loll)
+
+
+x = lambda a, b, c:  f"{a},{b} {c}"
+
+print( x(1,2,3) )
